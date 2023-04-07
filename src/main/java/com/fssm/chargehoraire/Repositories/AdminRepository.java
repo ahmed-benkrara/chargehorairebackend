f7d8a88a3175fd704e72079363184027200c76c0
ahmed-benkrara <ahmed.benkrara12@gmail.com>
2023-04-07 07:59:48 +0200

    adding services

--- src/main/java/com/fssm/chargehoraire/Repositories/AdminRepository.java
+++ src/main/java/com/fssm/chargehoraire/Repositories/AdminRepository.java
@@ -4,4 +4,5 @@ import com.fssm.chargehoraire.Models.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByEmailAndPassword(String email, String password);
 }
--- src/main/java/com/fssm/chargehoraire/Services/AdminService.java
+++ src/main/java/com/fssm/chargehoraire/Services/AdminService.java
@@ -1,4 +1,20 @@
 package com.fssm.chargehoraire.Services;
 
+import com.fssm.chargehoraire.Models.Admin;
+import com.fssm.chargehoraire.Repositories.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AdminService {
+    @Autowired
+    private AdminRepository adminRepository;
+
+    public Admin create(Admin admin){
+        return adminRepository.save(admin);
+    }
+
+    public Admin readByEmailAndPassword(String email, String password){
+        return adminRepository.findByEmailAndPassword(email, password);
+    }
 }
